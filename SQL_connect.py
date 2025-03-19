01c0680b1ebcf4ffb617c813cc0fd518105c47d3
Mads B. Kjær <mbk1dk@gmail.com>
2025-03-19 14:35:51 +0100

    Create SQL_connect.py
    Create connection, cursor and create / reset database

---
+++ SQL_connect.py
@@ -0,0 +1,53 @@
+from environs import env
+import mysql.connector as sql
+import csv
+from os import path
+
+
+class ConnectSQL:
+    env_dict: dict[str, str]
+    database_info: dict[str, list[str]]
+
+    def __init__(self, env_key: str, reset_database: bool = False) -> None:
+        self.env_dict = env.dict(env_key)
+        self.database_info = {}
+
+        if reset_database:
+            self.reset_database()
+        else:
+            self.create_connection(True)
+
+    def close_all(self) -> None:
+        self.cursor.close()
+        self.connection.close()
+
+    def create_connection(self, to_database: bool) -> None:
+        try:
+            if to_database:
+                self.connection = sql.connect(**self.env_dict)
+            else:
+                self.connection = sql.connect(
+                    user=self.env_dict["user"],
+                    password=self.env_dict["password"],
+                    host=self.env_dict["host"],
+                    port=self.env_dict["port"],
+                )
+            self.create_cursor()
+
+        except Exception as error:
+            print(f"Error creating connection:\n\t", error)
+
+    def create_cursor(self) -> None:
+        try:
+            self.cursor = self.connection.cursor()
+        except Exception as error:
+            print(f"Error creating cursor:\n\t", error)
+
+    def commit(self) -> None:
+        self.connection.commit()
+    def reset_database(self) -> None:
+        self.create_connection(False)
+        self.run_query(
+            f"drop database if exists {self.env_dict["database"]}; create database if not exists {self.env_dict["database"]};"
+        )
+        self.create_connection(True)
